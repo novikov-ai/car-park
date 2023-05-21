425984f01771ffd0eccc05dd1236f9144dc1fa20
ainovikov <ainovikov@avito.ru>
2023-05-21 23:02:08 +0300

    add migration

---
+++ db/fixtures/init.sql
@@ -0,0 +1,5 @@
+INSERT INTO vehicle (price, manufacture_year, mileage, color, vin)
+    VALUES
+        (1500000, 2015, 75000, 2, '1HGFA16836L135699'),
+        (4900000, 2021, 5040, 1, '1FTPX17L71KB86187'),
+        (645000, 2012, 210000, 3, '5XXGM4A76DG184578');
