f0922bc153bf9355b83a74aa3c0490490cfa7547
ainovikov <ainovikov@avito.ru>
2023-05-21 23:00:30 +0300

    add env example

---
+++ db/changelog/01-init-vehicle.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS vehicle (
+    id                      bigserial       PRIMARY KEY,
+    price                   int             NOT NULL,
+    manufacture_year        int             NOT NULL,
+    mileage                 int             NOT NULL,
+    color                   int             NOT NULL,
+    vin                     text            NOT NULL,
+    created_at              timestamptz     NOT NULL DEFAULT now(),
+    updated_at              timestamptz     NOT NULL DEFAULT now(),
+    deleted_at              timestamptz     NULL
+);
+
+CREATE INDEX IF NOT EXISTS idx_vehicle_created_at ON vehicle (created_at);
+CREATE INDEX IF NOT EXISTS idx_vehicle_updated_at ON vehicle (updated_at);
+CREATE INDEX IF NOT EXISTS idx_vehicle_deleted_at ON vehicle (deleted_at);
