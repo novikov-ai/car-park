a02863b1cf6fb75f5e39de47e09c0110424905cb
ainovikov <ainovikov@avito.ru>
2023-05-30 00:39:30 +0300

    add controller for models

---
+++ internal/controllers/models/provider.go
@@ -0,0 +1,54 @@
+package models
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"car-park/internal/models"
+	"car-park/internal/storage"
+)
+
+type Provider struct {
+	db storage.Client
+}
+
+func New(st storage.Client) *Provider {
+	return &Provider{
+		db: st,
+	}
+}
+
+func (p *Provider) FetchAllModels(ctx context.Context) []models.Model {
+	query := `SELECT * FROM model`
+	resp, err := p.db.Query(ctx, query)
+	if err != nil {
+		panic(err)
+	}
+
+	var (
+		id                                        int64
+		brand                                     string
+		vehicleType, seats, tank, vehicleCapacity int
+	)
+
+	mm := make([]models.Model, 0)
+	for resp.Next() {
+		err = resp.Scan(&id, &brand, &vehicleType, &seats, &tank, &vehicleCapacity)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "scan failed: %v\n", err)
+			continue
+		}
+
+		mm = append(mm, models.Model{
+			ID:              id,
+			BrandName:       brand,
+			VehicleType:     vehicleType,
+			Seats:           seats,
+			Tank:            tank,
+			VehicleCapacity: vehicleCapacity,
+		})
+	}
+
+	return mm
+}
