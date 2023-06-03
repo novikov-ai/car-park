f085262caf6bbc5c19d2f9a54900a5abac8200b0
ainovikov <ainovikov@avito.ru>
2023-06-03 23:41:24 +0300

    add constraints & field

--- db/changelog/03-init-vehicle.sql
+++ db/changelog/03-init-vehicle.sql
@@ -1,17 +1,18 @@
 CREATE TABLE IF NOT EXISTS vehicle (
     id                      bigserial       PRIMARY KEY,
     model_id                bigint          NOT NULL,
+    enterprise_id           bigint          NULL,
     price                   int             NOT NULL,
     manufacture_year        int             NOT NULL,
     mileage                 int             NOT NULL,
     color                   int             NOT NULL,
-    vin                     text            NOT NULL,
+    vin                     text            NOT NULL UNIQUE,
     created_at              timestamptz     NOT NULL DEFAULT now(),
     updated_at              timestamptz     NOT NULL DEFAULT now(),
-    deleted_at              timestamptz     NULL
+    deleted_at              timestamptz     NULL,
+    FOREIGN KEY (model_id) REFERENCES model (id),
+    FOREIGN KEY (enterprise_id) REFERENCES enterprise (id)
 );
-
-FOREIGN KEY (model_id) REFERENCES model (id);
 
 CREATE INDEX IF NOT EXISTS idx_vehicle_created_at ON vehicle (created_at);
 CREATE INDEX IF NOT EXISTS idx_vehicle_updated_at ON vehicle (updated_at);
